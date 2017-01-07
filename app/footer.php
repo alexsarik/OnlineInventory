00a4dd8c0a6fb9f5868301324048eb3f8bc21637
rozularen <nowitzki3>
2017-01-07 05:27:22 +0100

    Navbar working

--- app/footer.php
+++ app/footer.php
@@ -1,10 +1,7 @@
     </div>
     <!-- /container -->
  
-<!-- jQuery library -->
-<script src="libs/js/jquery.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-  
+
 <!-- HTML5 Shiv and Respond.js IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]>
