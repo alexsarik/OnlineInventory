95d1ea03f8507601cf735b299cb7922efc8c68e1
Marcos Stival <markcosta15@hotmail.com>
2017-01-14 13:57:53 +0100

    Ventas creada. Vamos a ver si listamos una venta.

--- app/view/sales/list_sales.php
+++ app/view/sales/list_sales.php
@@ -6,3 +6,42 @@
  * Time: 13:25
  */
 ?>
+<section>
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="#">Ventas</a></li>
+        <li class="breadcrumb-item active">Ver Ventas</li>
+    </ol>
+
+    <div class='right-button-margin'>
+        <a href='index.php?c=sale&a=create' class='btn btn-success pull-right'>Añadir Venta</a>
+    </div>
+
+    <h2>Lista de Ventas</h2>
+    <p>Mostrando <?= count($sales) ?> venta(s).</p>
+    <div class="table-default" style="overflow-x: scroll">
+        <table class="table table-hover table-responsive table-bordered">
+            <thead>
+            <th>Fecha</th>
+            <th>Cliente</th>
+            <th>Vendedor</th>
+            <th>Acciones</th>
+            </thead>
+            <tbody>
+            <?php foreach ($sales as $sale) : ?>
+                <tr id='sale<?= $sale->id ?>'>
+                    <td><?= $sale->serial ?></td>
+                    <td><?= $sale->description ?></td>
+                    <td><?= $sale->model ?></td>
+                    <td><?= $sale->location ?></td>
+                    <td style='white-space:nowrap'>
+                        <a href='index.php?c=sale&a=details&id=<?= $sale->id ?>' class='btn btn-primary left-margin'><span class='glyphicon glyphicon-list'></span> Detalles</a>
+                        <a href='index.php?c=sale&a=update&id=<?= $sale->id ?>' class='btn btn-info left-margin'><span class='glyphicon glyphicon-list'></span> Modificar</a>
+                        <a href='index.php?c=sale&a=delete&id=<?= $sale->id ?>' class='btn btn-danger delete-object'><span class='glyphicon glyphicon-list'></span> Eliminar</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+
+            </tbody>
+        </table>
+    </div>
+</section>
